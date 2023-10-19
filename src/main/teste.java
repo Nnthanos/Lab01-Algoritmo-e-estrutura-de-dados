19791ac8c586076f0b6f177183eda0640d108716
Nathan Cunha <nathancunha38@gmail.com>
2023-10-19 20:19:10 -0300

    finalizando os métodos "maior" e "parMaisProximo". OBS: Não consegui fazer funcionar os testes do parMaisProximo mas deixei uma classe teste.java para printar os resultados

--- src/main/teste.java
+++ src/main/teste.java
@@ -2,8 +2,13 @@ package main;
 
 public class teste {
     public static void main(String[] args) {
-        System.out.println(App.parMaisProximo(new Integer[][] { new Integer[] { 2, 4 }, new Integer[] { 5, 9 },
-                new Integer[] { 1, 3 }, new Integer[] { 8, 2 }, new Integer[] { 2, 4 } }));
+        System.out.println("Era esperado 1.414:\n"
+                + App.parMaisProximo(new Integer[][] { new Integer[] { 2, 4 }, new Integer[] { 5, 9 },
+                        new Integer[] { 1, 3 }, new Integer[] { 8, 2 }, new Integer[] { 6, 7 } }));
 
+        System.out.println("Era esperado null:\n" + App.parMaisProximo(new Integer[][] {}));
+        
+        System.out.println("Era esperado 0.0:\n" + App.parMaisProximo(new Integer[][] { new Integer[] { 2, 4 },
+                new Integer[] { 5, 9 }, new Integer[] { 1, 3 }, new Integer[] { 8, 2 }, new Integer[] { 2, 4 } }));
     }
 }
