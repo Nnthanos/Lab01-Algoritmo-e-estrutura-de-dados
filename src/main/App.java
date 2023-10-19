c09fe0ea13035a44a96c6529e5280efcc9fb05bf
Nathan Cunha <nathancunha38@gmail.com>
2023-10-19 20:14:50 -0300

    finalizando os métodos "maior" e "parMaisProximo". OBS: Não consegui fazer funcionar os testes do parMaisProximo mas deixei uma classe teste.java para printar os resultados

--- src/main/App.java
+++ src/main/App.java
@@ -1,15 +1,16 @@
 package main;
+
 public class App {
-    public static int encontre(int[] arrayOrdenado, int num){
+    public static int encontre(int[] arrayOrdenado, int num) {
         int indiceEsquerda = 0;
         int indiceDireita = arrayOrdenado.length - 1;
 
-        while (indiceEsquerda <= indiceDireita){
+        while (indiceEsquerda <= indiceDireita) {
             int indiceMetade = (indiceDireita + indiceEsquerda) / 2;
             int metade = arrayOrdenado[indiceMetade];
-            if (num == metade){
+            if (num == metade) {
                 return 1;
-            } else if(num > metade){
+            } else if (num > metade) {
                 indiceEsquerda += indiceMetade;
             } else {
                 indiceDireita -= indiceMetade;
@@ -18,11 +19,38 @@ public class App {
         return 0;
     }
 
-    public static int maior(int[] arrayDesordenado){
-        return -1;
+    public static int maior(int[] arrayDesordenado) {
+        int maior = arrayDesordenado[0];
+        for (int i : arrayDesordenado) {
+            if (i > maior) {
+                maior = i;
+            }
+        }
+        return maior;
     }
 
-    public static Integer parMaisProximo(Integer[][] pares){
-        return 1;
+    public static Double parMaisProximo(Integer[][] pares) {
+
+        if (pares.length == 0) {
+            return null;
+        }
+
+        double menorDistancia = -1;
+        for (int i = 0; i < pares.length; i++) {
+            Integer x1 = pares[i][0];
+            Integer y1 = pares[i][1];
+            for (int j = i + 1; j < pares.length; j++) {
+
+                Integer x2 = pares[j][0];
+                Integer y2 = pares[j][1];
+
+                double distancia = Math.sqrt(Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2)));
+
+                if (menorDistancia == -1 || menorDistancia > distancia) {
+                    menorDistancia = distancia;
+                }
+            }
+        }
+        return menorDistancia;
     }
 }
---
+++ src/main/teste.java
@@ -0,0 +1,9 @@
+package main;
+
+public class teste {
+    public static void main(String[] args) {
+        System.out.println(App.parMaisProximo(new Integer[][] { new Integer[] { 2, 4 }, new Integer[] { 5, 9 },
+                new Integer[] { 1, 3 }, new Integer[] { 8, 2 }, new Integer[] { 2, 4 } }));
+
+    }
+}
